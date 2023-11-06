66f00f5d3304dc92b7a876f5b80fcf688b6ecd60
Egli <eglimiraka7@gmail.com>
2023-11-07 00:31:50 +0100

    RentalPro

---
+++ src/assets/data/serviceData.js
@@ -0,0 +1,22 @@
+const serviceData = [
+  {
+    id: 1,
+    title: "Parking",
+    icon: "ri-map-pin-2-line",
+    desc: "Secure and convenient 24-hour parking for your vehicles. Your peace of mind, our priority.",
+  },
+  {
+    id: 2,
+    title: "Car Sale",
+    icon: "ri-car-line",
+    desc: "Explore a wide range of quality vehicles, from brand new to certified pre-owned. Find your dream car with us.",
+  },
+  {
+    id: 3,
+    title: "Car Rental",
+    icon: "ri-roadster-line",
+    desc: "Rent a car for your adventures and journeys. Quality cars for short-term and long-term rentals.",
+  }
+];
+
+export default serviceData;
