9c0f65b2e33b815f73ea45113e9e0391dd2a2a82
eglimiraka22 <eglimiraka7@gmail.com>
2023-12-17 15:15:59 +0100

    update

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './helmet.css'
 const Helmet = (props) => {
-  document.title = "Rent Car Service - " + props.title;
+  document.title = "Rent Pro - " + props.title;
   return <div className="w-100 home-page">{props.children}</div>;
 };
 
